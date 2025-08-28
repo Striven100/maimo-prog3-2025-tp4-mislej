6a8c6e62536b216f2388412f7572c1a916d7119c
Tobias Mislej <tobimislej@gmail.com>
2025-08-28 18:28:42 -0300

    shop

---
+++ src/app/favorites/page.js
@@ -0,0 +1,10 @@
+import FavoritesContainer from '@/components/FavoritesContainer'
+import React from 'react'
+
+const Favorite = () => {
+  return (
+    <FavoritesContainer />
+  )
+}
+
+export default Favorite
